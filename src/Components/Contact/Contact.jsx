5b5353d6b5d21d51ad529ee80e68eae7b7c65ebf
Serhii-Kysil <serik.kisel000@gmail.com>
2024-01-27 18:02:00 +0200

    Add Task

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,27 @@
+import { BsFillPersonFill } from "react-icons/bs";
+import { FaPhone } from "react-icons/fa6";
+import css from "./Contact.module.css";
+
+export const Contact = ({ contact, onDeleteContact }) => {
+  const handleDeleteContact = () => {
+    onDeleteContact(contact.id);
+  };
+  return (
+    <li className={css.listItem}>
+      <div className={css.itemCont}>
+        <p className={css.par}>
+          <BsFillPersonFill /> {contact.name}
+        </p>
+        <p className={css.par}>
+          {" "}
+          <FaPhone /> {contact.number}
+        </p>
+      </div>
+      <div className={css.btnCont}>
+        <button className={css.btn} onClick={handleDeleteContact}>
+          Delete
+        </button>
+      </div>
+    </li>
+  );
+};
---
+++ src/Components/ContactList/ContactList.jsx
@@ -0,0 +1,16 @@
+import { Contact } from "../Contact/Contact";
+import css from "./ContactList.module.css";
+
+export const ContactList = ({ contacts, onDeleteContact }) => {
+  return (
+    <ul className={css.list}>
+      {contacts.map((contact) => (
+        <Contact
+          key={contact.id}
+          contact={contact}
+          onDeleteContact={onDeleteContact}
+        />
+      ))}
+    </ul>
+  );
+};
